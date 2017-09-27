bc9ae11cf2ad1d8c4b714180ad67487daa9ef2d8
adaisti <Ada@Ada-Maaria-Hyvarinen-MacBook.local>
2017-09-27 11:20:09 +0300

    muutoksia

--- koodit/vertaile.py
+++ koodit/vertaile.py
@@ -11,14 +11,46 @@ import numpy as np
 
 
 def print_words():
-	f = open('/home/adahyvar/gradu/testidata/combined.tab', 'r')
+	#f = open('/home/adahyvar/gradu/testidata/combined.tab', 'r')
+	#f = open('/Users/Ada/gradu/testidata/combined.tab', 'r')
+
+	f = open('/Users/Ada/gradu/testidata/suomi1.tsv', 'r')
+
 	lines = f.read()
 	
 	for line in lines.split("\n"):
+		if line == "":
+			continue
 		word1 = line.split("\t")[0]
 		word2 = line.split("\t")[1]
 		print(word1)
 		print(word2)
+
+
+def similarities_words(lines2, vectors):
+
+	words = []
+
+	for line in lines2.split("\n"):
+		if line == "":
+			continue
+
+		word1 = line.split("\t")[0]
+		word2 = line.split("\t")[1] 
+		words.append(word1 + " " + word2)
+
+	similarities = []
+
+	for i in range(0,len(vectors) - 2,2):
+		similarities.append(1 - cosine(vectors[i], vectors[i + 1]))
+
+	similarities = similarities[1:]	#1 koska eka rivi on sellainen turha
+	words = words[1:]
+
+	#return similarities, words
+	for i in range(len(similarities)):
+		print(words[i], similarities[i])
+
 
 def main():
 
@@ -34,7 +66,14 @@ def main():
 	#	print(word1, word2) 
 
 	#f = open('/home/adahyvar/gradu/testidata/set2_vectors.txt', 'r')
-	f = open('/home/adahyvar/gradu/testidata/combined_vectors.txt', 'r')
+	#f = open('/home/adahyvar/gradu/testidata/combined_vectors.txt', 'r')
+	
+	#f = open('/Users/Ada/gradu/testidata/combined_vectors.txt', 'r')
+	#f = open('/Users/Ada/gradu/testidata/set2_suomi_vektorit.txt', 'r')
+	f = open('/Users/Ada/gradu/testidata/combined_vectors.txt', 'r')
+	
+	
+
 	lines = f.read()
 
 	vectors = []
@@ -47,12 +86,24 @@ def main():
 
 	scores = []
 
-	words = []
 
 	#f2 = open('/home/adahyvar/gradu/testidata/set2_kaannos_utf-8.tsv', 'r')
-	f2 = open('/home/adahyvar/gradu/testidata/combined.tab', 'r')
+	#f2 = open('/home/adahyvar/gradu/testidata/combined.tab', 'r')
 	
+	f2 = open('/Users/Ada/gradu/testidata/combined.tab', 'r')
+	#f2 = open('/Users/Ada/gradu/testidata/suomi1.tsv', 'r')
+	#f2 = open('/Users/Ada/gradu/testidata/set2_kaannos_utf-8.tsv', 'r')
+
+
 	lines2 = f2.read()
+
+	#similarities_words(lines2, vectors)
+
+	#return
+
+	words = []
+
+
 
 	for line in lines2.split("\n"):
 
@@ -61,7 +112,9 @@ def main():
 		score = line.split("\t")[2]
 		scores.append(score)
 		word1 = line.split("\t")[0]
-		word2 = line.split("\t")[1] 
+		word2 = line.split("\t")[1]
+		#word1 = line.split("\t")[3]
+		#word2 = line.split("\t")[4]  
 		words.append(word1 + " " + word2)
 
 	similarities = []
